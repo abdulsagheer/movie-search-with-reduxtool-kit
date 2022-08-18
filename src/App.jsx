f5ea137b4336691a116fdd081a7e6dd1481e68d4
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-18 23:04:08 +0530

    Structured the project for movie app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Movie App</h1>
+      <h1>Movie App with Redux Toolkit Saga</h1>
     </div>
   );
 }
