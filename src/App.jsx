e88d6bc65381e0bf47e28a11c2116568f6833f40
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-19 14:17:26 +0530

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
 
 function App() {
   return (
     <div className="App">
       <h1>Movie App with Redux Toolkit Saga</h1>
+      <Link to="/">Home</Link>
+      <Link to="/movie/:id">Movie</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Movie />} />
+      </Routes>
     </div>
   );
 }
